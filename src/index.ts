9448a8b3a4fc7952021526525bd254859607f2c0
roydukkey <contact@changelog.me>
2021-07-07 12:59:15 -0400

    Simplify type param signature

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,6 @@
  * @param context - The value to forward to the scope function.
  * @param scope - The function which will receive the given context.
  */
-export default function <C, S extends (context: C) => JSX.Element> (context: C, scope: S): JSX.Element {
+export default function <T> (context: T, scope: (context: T) => JSX.Element): JSX.Element {
 	return scope(context);
 }
