da0365e7eb3dbd5a6c0f75c98017ef6ef1c3b55c
roydukkey <contact@changelog.me>
2021-07-07 12:48:48 -0400

    Update doc

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 /**
  * Immediately invokes the provided function for the given context.
  *
- * @param context - The value to forward to the transform function.
+ * @param context - The value to forward to the scope function.
  * @param scope - The function which will receive the given context.
  */
 export default function <C, S extends (context: C) => JSX.Element> (context: C, scope: S): JSX.Element {
