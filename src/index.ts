eb92a22431945955a3289fce927b1967a56f198e
roydukkey <contact@changelog.me>
2021-08-09 11:28:03 -0400

    Update api

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 /**
- * Immediately invokes the provided function for the given context.
+ * Immediately invokes the provided function for the given context, when the context is not `null` or `undefined`.
  *
  * @param context - The value to forward to the scope function.
  * @param scope - The function which will receive the given context.
+ * @returns When the context is `null` or `undefined`, `undefined` is returned; otherwise, the {@link JSX.Element} received from the scope's invocation is returned.
  */
-export default function <T> (context: T, scope: (context: T) => JSX.Element): JSX.Element {
-	return scope(context);
+ export default function <T> (context: T | undefined | null, scope: (context: T) => JSX.Element): JSX.Element | undefined {
+	if (context !== null && context !== undefined) {
+		return scope(context);
+	}
 }
